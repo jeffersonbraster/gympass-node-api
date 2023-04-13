de7be788a578f586b1430e32c74c2f0a05b658d2
Jefferson <jeferson.oliveira@g.globo>
2023-04-12 22:28:17 -0300

    config eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import {z} from 'zod'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
@@ -8,10 +8,9 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if(!_env.success) {
+if (!_env.success) {
   console.error('❌ Invalid env', _env.error.format())
   throw new Error('Invalid env')
 }
 
 export const env = _env.data
-
