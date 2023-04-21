bd2d8f9656a937b092e172552108311a7335892d
Jefferson <jeferson.oliveira@g.globo>
2023-04-21 00:24:18 -0300

    config coverage

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -35,7 +35,7 @@ describe('Register use case', () => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
         email,
