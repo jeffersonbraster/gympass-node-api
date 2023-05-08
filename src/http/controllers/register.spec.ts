53ea6abd2fa49da91366c1d1cd00db0b6cdcac68
Jefferson <jefferson.brandao@g.globo>
2023-05-08 17:06:03 -0300

    test e2e de register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Jeje',
+      email: 'jeje@gmail.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(201)
+  })
+})
